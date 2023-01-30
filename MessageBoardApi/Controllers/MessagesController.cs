528cd894cfa0f93b9bbc59fe449726f6f32d1512
Hans Ellis <bullhorn@gmail.com>
2023-01-29 16:41:45 -0800

    add get and post actions to messagescontroller
    Co-authored-by: Oscar Gonzalez <ogon318@gmail.com>
    Co-authored-by: Quin Asselin <quin.asselin1@gmail.com>
    Co-authored-by: Remy Flores <remyfranciscoflores@gmail.com>

---
+++ MessageBoardApi/Controllers/MessagesController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MessageBoardApi.Models;
+
+namespace MessageBoardApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class MessagesController : ControllerBase
+  {
+    private readonly MessageBoardApiContext _db;
+
+    public MessagesController(MessageBoardApiContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Message>>> Get()
+    {
+      return await _db.Messages.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Message>> GetMessage(int id)
+    {
+      Message message = await _db.Messages.FindAsync(id);
+
+      if (message == null)
+      {
+        return NotFound();
+      }
+      
+      return message;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Message>> Post(Message message)
+    {
+      _db.Messages.Add(message);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetMessage), new { id = message.MessageId }, message);
+    }
+  }
+}
