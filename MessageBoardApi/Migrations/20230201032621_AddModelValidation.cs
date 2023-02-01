3bf5e47449b6102ed96d13f8b0869a523c39f20e
Hans Ellis <bullhorn@gmail.com>
2023-01-31 19:28:43 -0800

    update migrations with model validation
    Co-authored-by: Oscar Gonzalez <ogon318@gmail.com>
    Co-authored-by: Quin Asselin <quin.asselin1@gmail.com>
    Co-authored-by: Remy Flores <remyfranciscoflores@gmail.com>

---
+++ MessageBoardApi/Migrations/20230201032621_AddModelValidation.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MessageBoardApi.Migrations
+{
+    public partial class AddModelValidation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "UserName",
+                keyValue: null,
+                column: "UserName",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Messages",
+                type: "varchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Group",
+                keyValue: null,
+                column: "Group",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Group",
+                table: "Messages",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Comment",
+                keyValue: null,
+                column: "Comment",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "Messages",
+                type: "varchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Messages",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(25)",
+                oldMaxLength: 25)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Group",
+                table: "Messages",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Comment",
+                table: "Messages",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(250)",
+                oldMaxLength: 250)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
