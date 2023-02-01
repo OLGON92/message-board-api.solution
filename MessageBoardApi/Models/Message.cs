d187ee00c645ce584a63714a0c3c11ea7ea3d854
Hans Ellis <bullhorn@gmail.com>
2023-01-31 19:27:45 -0800

    add validation to models
    Co-authored-by: Oscar Gonzalez <ogon318@gmail.com>
    Co-authored-by: Quin Asselin <quin.asselin1@gmail.com>
    Co-authored-by: Remy Flores <remyfranciscoflores@gmail.com>

--- MessageBoardApi/Models/Message.cs
+++ MessageBoardApi/Models/Message.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MessageBoardApi.Models
 {
   public class Message
   {
     public int MessageId { get; set; }
+    [Required]
+    [StringLength(250, ErrorMessage = "You must enter a comment!")]
     public string Comment { get; set; }
+    [Required]
+    [StringLength(50, ErrorMessage = "You must enter a group name! Or suffer the consequences!")]
     public string Group { get; set; }
+    [Required]
+    [StringLength(25, ErrorMessage = "Enter a username or you're gonna catch deez hands!")]
     public string UserName { get; set; }
     public DateTime Date { get; set; }
   }
